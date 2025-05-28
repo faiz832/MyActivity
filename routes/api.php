f85ece23e3a3f18e68bf23d1dd4be874dc29f36a
faiz832 <faqodefazz@gmail.com>
2025-05-28 12:32:12 +0700

    set api users

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::get('/logout', [AuthController::class, 'logout']);
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+
+    Route::apiResource('/users', UserController::class);
 });
 
 Route::post('/login', [AuthController::class, 'login']);
